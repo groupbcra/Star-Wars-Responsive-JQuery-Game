869acd2bfd9ca15d58722596dd563edddea1b18f
Cole Hansen <cole.hansen@gmail.com>
2018-03-18 12:34:32 -0700

    Add fighterObj with nested objects for each character. Also add link to JS and jQuery in index.html

---
+++ assets/js/starwars.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    // startScreen();
+
+    // Fighter stats
+    var fighterObj = {
+        obiWan: {
+            health: 100,
+            power: 120,
+            counter: 100,
+        },
+        luke: {
+            health: 100,
+            power: 120,
+            counter: 100,
+        },
+        palpatine: {
+            health: 100,
+            power: 120,
+            counter: 100,
+        },
+        maul: {
+            health: 100,
+            power: 120,
+            counter: 100,
+        },
+    };
+    // Checking that the objects can be called correctly
+    console.log("Luke's Health: " + fighterObj.luke.health);
+    console.log("Palpatine's Counter: " + fighterObj.palpatine.counter);
+
+    
+
+});
