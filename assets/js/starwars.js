39dbcc114ddc705c34efdd6cacd56aa526060e4c
Avi Davis Mac <oodavis.avi@gmail.com>
2018-03-18 18:02:30 -0700

    Just setup the js file a bit more towards the psuedo code

--- assets/js/starwars.js
+++ assets/js/starwars.js
@@ -50,7 +50,37 @@ var enemyObj = {
 enemyObj.luke = fighterObj.luke;
 console.log("This is the luke enemyObj health: " + enemyObj.luke.health);
 
+function gameSetup() {
+    console.log("I'm in gameSetup");
+    // function loadCharcters
+    // onclick chosen character is added to  
+    // set player power/health.100 value
+    //  remove rest of the Object to 
+    // onclick chosen defender  is added to  
+    // set defender power/health.100 value
+    startFight();
+}
+
+function startFight() {
+    //  var fighterHealth =  fighterObj.chosenFighter.Health
+    //  var fighterPower =  fighterObj.chosenFighter.Power
+    //  var enemyHealth =  enemyObj.chosenEnemy.Health
+    //  var enemyPower = enemyObj.chosenEnemy.Power
+    // onclick  $(attack) function
+}
+
+function attack() {
+    //var fighterNumb = Math.floor(Math.random() * fighterPower);
+    //var enemyNumb = Math.floor(Math.random() * enemyPower);
+   // fighterHealth -= enemyNumb
+    //enemyHeath -= fighterNumb
+}
+
+
+
+
 function startScreen() {
     //window.empty();  Brian depending on how you get your function done his might or might not be needed. As of now the HTML for the game is hard coded. Games generate the content. Let us know how you are going to do it. 
+    console.log("I'm in startScreen");
     gameSetup();
 }
